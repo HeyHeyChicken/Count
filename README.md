c4a42c4cf1e5652544980256c6c091517e475cc7
HeyHeyChicken <antoine@cuffel.fr>
2020-04-24 21:35:05 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,35 @@
+<div align="center">
+<a href="//nova-assistant.com" rel="nofollow">
+<img src="https://github.com/HeyHeyChicken/NOVA-Count/blob/master/resources/nova-logo.svg" alt="NOVA" width="300">
+</a>
+
+**NOVA - Count** is a skill for your [NOVA](//github.com/HeyHeyChicken/NOVA) assistant.<br>
+This skill will allow your assistant to count.
+
+<br>
+
+<img src="https://github.com/HeyHeyChicken/NOVA-Count/blob/master/resources/screenshot.jpg" alt="NOVA" width="800">
+</div>
+
+<br>
+
+## Installation
+
+1) Download it directly from GitHub.
+2) Put the "/src/" content of your downloaded skill, into your NOVA's "/src/skills/Random/" folder.
+
+## Features
+
+- It will allow your assistant to roll a die (give you a number between 1 and 6).
+- It will allow your assistant give you a number between x and y.
+
+## Support and contribution
+
+I provide support for all users through [GitHub issues](//github.com/HeyHeyChicken/NOVA-Count/issues).
+
+If you would like to contribute to this project, make sure you first read the [guide for contributors](//github.com/HeyHeyChicken/NOVA/blob/master/CONTRIBUTING.md).
+
+<br>
+<br>
+
+Created by [Antoine Duval (HeyHeyChicken)](//antoine.cuffel.fr) with ❤ and ☕ (chocolate) in [Déville-lès-Rouen](//en.wikipedia.org/wiki/Déville-lès-Rouen).
---
+++ src/index.js
@@ -0,0 +1,79 @@
+const LIBRARIES = {
+  Skill: require("../../../Skill")
+};
+
+class Count extends LIBRARIES.Skill{
+  constructor(_main, _settings) {
+    super(_main, _settings);
+    const SELF = this;
+
+    this.Main.Manager.addAction("count.up", function(_intent, _socket){
+      if(_intent.Variables.number !== undefined){
+        if(!isNaN(parseFloat(_intent.Variables.number))){
+          let string = "";
+          for(let index = 1; index <= _intent.Variables.number; index++){
+            string += index;
+            if(index < _intent.Variables.number){
+              string += ", ";
+            }
+            else{
+              string += ".";
+            }
+          }
+          _intent.answer(_socket, string);
+        }
+      }
+    });
+
+    this.Main.Manager.addAction("count.down", function(_intent, _socket){
+      if(_intent.Variables.number !== undefined){
+        if(!isNaN(parseFloat(_intent.Variables.number))){
+          let string = "";
+          for(let index = _intent.Variables.number; index >= 0; index--){
+            string += index;
+            if(index > 0){
+              string += ", ";
+            }
+            else{
+              string += ".";
+            }
+          }
+          _intent.answer(_socket, string);
+        }
+      }
+    });
+
+    this.Main.Manager.addAction("count.from.to", function(_intent, _socket){
+      if(_intent.Variables.from !== undefined && _intent.Variables.to !== undefined){
+        if(!isNaN(parseFloat(_intent.Variables.from)) && !isNaN(parseFloat(_intent.Variables.to))){
+          let string = "";
+          if(_intent.Variables.to < _intent.Variables.from){
+            for(let index = _intent.Variables.from; index >= _intent.Variables.to; index--){
+              string += index;
+              if(index > _intent.Variables.to){
+                string += ", ";
+              }
+              else{
+                string += ".";
+              }
+            }
+          }
+          else{
+            for(let index = _intent.Variables.from; index <= _intent.Variables.to; index++){
+              string += index;
+              if(index < _intent.Variables.to){
+                string += ", ";
+              }
+              else{
+                string += ".";
+              }
+            }
+          }
+          _intent.answer(_socket, string);
+        }
+      }
+    });
+  }
+}
+
+module.exports = Count;
